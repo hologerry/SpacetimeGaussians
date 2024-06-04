9f258ea2c11584d61a2431bec0f25386aeb31195
Yue Gao <hologerry@gmail.com>
2024-06-05 02:23:58 +0800

    two_sp_level_couple update

--- script/test_two_sp_level.sh
+++ script/test_two_sp_level.sh
@@ -3,8 +3,8 @@
 python test_two_sp_level.py \
     --test \
     --source_path /dev/shm/ScalarReal \
-    --config configs/gaussian_fluid/scalar_real_simple_color_scale_rotation_two_sp_level_act.json \
-    --model_path /data/Dynamics/SpacetimeGaussiansLog/gaussian_fluid_scalar_real/two_sp_level_L1_co-sc-ro-tci0_N120x10_csp_densi0-5e3_pprune_L2_N120x100-op01-co06-sc--5_csp_densi5e3-1e4_noact_pruneco01 \
+    --config configs/gaussian_fluid/scalar_real_simple_color_scale_rotation_act_two_sp_level.json \
+    --model_path /data/Dynamics/SpacetimeGaussiansLog/gaussian_fluid_scalar_real/two_sp_level_L1_co-sc-ro-tci0_N120x10_csp_densi0-5e3_pprune_L2_N120x100-op01-co06-sc--5-xyzo0_csp_densi5e3-1e4_noact_fixlr \
     --val_loader hyfluid_valid
 exit
 }
