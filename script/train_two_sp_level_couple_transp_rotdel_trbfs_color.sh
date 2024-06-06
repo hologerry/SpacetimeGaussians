c596300b578c69fdf4d6d06349994e51a7eb51eb
Yue Gao <hologerry@gmail.com>
2024-06-06 17:00:16 +0800

    update config

--- script/train_two_sp_level_couple_transp_rotdel_trbfs_color.sh
+++ script/train_two_sp_level_couple_transp_rotdel_trbfs_color.sh
@@ -3,7 +3,7 @@
 python train_two_sp_level.py \
     --source_path /dev/shm/ScalarReal \
     --config configs/gaussian_fluid/scalar_real_simple_color_scale_rotation_act_two_sp_level_couple_transp_rotdel_trbfs_color.json \
-    --model_path /data/Dynamics/SpacetimeGaussiansLog/gaussian_fluid_scalar_real/two_sp_level_couple_transp_rotdel_trbfs2_color_L1_default_L2_default-delrotrs1vrand10_lrr0025_lrv002 \
+    --model_path /data/Dynamics/SpacetimeGaussiansLog/gaussian_fluid_scalar_real/two_sp_level_couple_transp_rotdel_trbfs2_color_L1_default_L2_default-delrot-xyz-rs1-vrand10_lrr0025_lrv002 \
     --loader hyfluid
 exit
 }
